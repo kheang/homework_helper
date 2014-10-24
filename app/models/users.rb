0687ea73879de28c31d05525dd56a216883e058d
Kheang Lim <kheang@gmail.com>
2014-10-24 14:55:54 -0400

    Added basic validation and tests to users table

--- app/models/users.rb
+++ app/models/users.rb
@@ -1,4 +1,17 @@
 class Users < ActiveRecord::Base
 	has_many :problems
 	has_many :notes
+
+	validates :name,
+            presence: true
+
+	validates :email,
+            presence: true,
+	          uniqueness: { case_sensitive: false },
+	          format: { with: /\A[\w\-\.]+@[\w\-\.]+\Z/, message: "must be a valid email address" }
+
+	validates :password_digest,
+            presence: true
+
+	has_secure_password
 end
--- test/models/users_test.rb
+++ test/models/users_test.rb
@@ -1,7 +1,16 @@
 require 'test_helper'
 
 class UsersTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+	should validate_presence_of(:name)
+
+	should validate_presence_of(:email)
+	should validate_uniqueness_of(:email).case_insensitive
+	should_not allow_value("BAD EMAIL").for(:email)
+	should_not allow_value("@").for(:email)
+	should_not allow_value("  example@example.com").for(:email)
+
+	should have_secure_password
+
+	# should have_many(:problems)
+	# should have_many(:notes)
 end
