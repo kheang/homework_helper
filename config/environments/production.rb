7f81b007526d2ff246ca82d9126ff480c61235b6
Kheang Lim <kheang@gmail.com>
2014-10-26 17:10:31 -0400

    added host to proper config file

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -69,6 +69,8 @@ Rails.application.configure do
       authentication:       'plain',
       enable_starttls_auto: true  }
 
+  config.action_mailer.default_url_options = { :host => 'immense-beach-8191.herokuapp.com' }
+
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).
   config.i18n.fallbacks = true
